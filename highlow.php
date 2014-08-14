3bdbaf769c2f1cab1aa4c3927db31f8d47df5b5c
Frank Rodriguez <frank.aaron.rodriguez@gmail.com>
2014-08-14 15:09:19 -0500

    Added support for terminal arguments

--- highlow.php
+++ highlow.php
@@ -1,40 +1,58 @@
 <?php
 
-$answer = mt_rand(1, 100);
 
-// echo $answer;
+//check if arguments are present
+if ($argc == 3) {
+	
+	if ($argv[1] < $argv[2]) { // prevents mt_rand errors if users set maximums lower than mins
+		$max = $argv[2];
+		$min = $argv[1];
+	} else {
+		fwrite(STDOUT, "Minimum is higher than maximum. Please run again with correct arguments. \n");
+		exit(0);
+	}
+
+} else {
+	$max = 100;
+	$min = 1; 	
+}
+
+$answer = mt_rand($min, $max);
 
 $num_of_guesses = 0;
 
-fwrite(STDOUT, 'Guess a number between 1 and 100... ');
+fwrite(STDOUT, "Guess a number between $min and $max... ");
 
-// var_dump($guess);
 $correct = false;
-
-
-
-//check answer
 
 do {
 	//take user input
 	$guess = trim(fgets(STDIN));
+	
 	$num_of_guesses++;
-if($guess == $answer) {
-	// usleep(3000000);
-	fwrite(STDOUT, "GOOD GUESS\nIt took you $num_of_guesses tries\n ");
-	$correct = true;
-	exit(0);
-} elseif ($guess > $answer && $guess <= 100) {
-	// usleep(3000000);
-	fwrite(STDOUT, "LOWER\nGUESS AGAIN\n");
-} elseif ($guess < $answer && $guess > 0) {
-	// usleep(3000000);
-	fwrite(STDOUT, "HIGHER\nGUESS AGAIN\n");
-} else {
-	fwrite(STDOUT, "Only enter numbers between 1 and 100\nGUESS AGAIN\n");
-}
+
+	if($guess == $answer) {
+
+		fwrite(STDOUT, "GOOD GUESS\nIt took you $num_of_guesses tries\n ");
+		$correct = true;
+		fwrite(STDOUT, "Would you like to play again?\ny / n\n");
+		$response = trim(fgets(STDIN));
+		
+		if ($response == 'y') {
+			$correct = false;
+			$answer = mt_rand($min, $max);
+			$num_of_guesses = 0;
+			fwrite(STDOUT, "Guess a number between $min and $max... \n");
+		} else {
+			fwrite(STDOUT, "Goodbye...\n");
+		}
+
+}	elseif ($guess > $answer && $guess <= $max) {
+		fwrite(STDOUT, "LOWER\nGUESS AGAIN\n");
+} 	elseif ($guess < $answer && $guess >= $min) {
+		fwrite(STDOUT, "HIGHER\nGUESS AGAIN\n");
+} 	else {
+		fwrite(STDOUT, "Only enter numbers between $min and $max\nGUESS AGAIN\n");
+} 
+
 } while(!$correct);
-
-//if guess is correct output correct and end the game
-
-//if guess is incorrect tell user if answer is higher or lower than guess and have another input
